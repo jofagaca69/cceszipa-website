be258f0a9bb712acf2bc8b50adc44de7c4784ed7
jofagaca69 <jfgarciac@unal.edu.co>
2025-06-25 13:30:34 -0500

    Configuración inicial de Astro con integración de Tailwind CSS y plugins para sitemap y robots.txt. Se añadieron componentes de navegación y SEO, así como estilos globales. Se crearon nuevos archivos SVG para logotipos y se implementó la estructura básica de la aplicación.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: 'https://colexploradoresdelsaber.com',
+  integrations: [sitemap(), robotsTxt()]
+});
